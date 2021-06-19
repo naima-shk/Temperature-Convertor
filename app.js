8367c2b848fe06120708ad2347b26383e3412426
naima-shk <naimashaikh550@gmail.com>
2021-06-19 23:16:16 +0500

    'temperature-convertor'

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const tempCalc= () =>{
-    let   numberTemp= document.getElementById('temp').value;
-    //console.log(numberTemp);
-    let  tempSelected = document.getElementById('unit').value;
-   let  valueTemp = unit.options[tempSelected.selectedIndex].value;
-    //console.log(valueTemp);
-      function celToFah(cel){
+function tempCalc() {
+    const  numberTemp= document.getElementById('temp').value;
+    console.log(numberTemp);
+    const  tempSelected = document.getElementById('unit').value;
+   const valueTemp = unit.options[tempSelected.selectedIndex].value;
+    console.log(valueTemp);
+      const  celToFah=(cel)=>{
         let fehrenhiet = Math.round((cel *9/5) +32);
         return fehrenhiet;
       }
-      function fehToCel(feh){
+     const  fehToCel=(feh)=>{
         let celsius = Math.round((feh - 32) *5/9);
         return  celsius;
       }
